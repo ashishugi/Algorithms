f4fb051fc5ab4c94239d1b14a6293683adb52c23
Arjit kesarwani <38940981+1Arjit007@users.noreply.github.com>
2020-05-14 08:22:36 +0530

    linear search

---
+++ Linear search.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int search(int a[],int n,int x)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==x)
+        {
+            return i+1;
+        }
+    }
+    return -1;
+}
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,k;
+	    cin>>n>>k;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	    }
+	    cout<<search(a,n,k)<<endl;
+	}
+	return 0;
+}
